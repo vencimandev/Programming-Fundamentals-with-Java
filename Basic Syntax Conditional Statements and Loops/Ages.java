5c5905fd0edc87caa1f85b5d0bdffd9735157faf
Ventsislav Mandev <115660651+vencimandev@users.noreply.github.com>
2024-01-28 12:06:12 +0200

    Add files via upload

---
+++ Basic Syntax Conditional Statements and Loops/Ages.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ages {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int age = scanner.nextInt();
+
+        String category = determineAgeCategory(age);
+
+        System.out.println(category);
+    }
+
+    private static String determineAgeCategory(int age) {
+        if (age >= 0 && age <= 2) {
+            return "baby";
+        } else if (age >= 3 && age <= 13) {
+            return "child";
+        } else if (age >= 14 && age <= 19) {
+            return "teenager";
+        } else if (age >= 20 && age <= 65) {
+            return "adult";
+        } else {
+            return "elder";
+        }
+    }
+}
---
+++ Basic Syntax Conditional Statements and Loops/PrintAndSum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class PrintAndSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int startNumber = Integer.parseInt(scanner.nextLine());
+        int endNumber = Integer.parseInt(scanner.nextLine());
+
+
+        int sum = 0;
+
+
+
+        for (int number = startNumber; number <= endNumber; number++) {
+            System.out.print(number + " ");
+            sum = sum + number;
+        }
+
+        System.out.println();
+        System.out.println("Sum: " + sum);
+
+    }
+}
