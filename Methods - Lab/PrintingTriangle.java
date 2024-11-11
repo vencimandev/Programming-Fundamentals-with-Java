89b590fa4179d0bd00f20d49ad7ef1fdc7f7d2b4
Ventsislav Mandev <115660651+vencimandev@users.noreply.github.com>
2024-11-11 19:02:55 +0200

    Add files via upload

---
+++ Methods - Lab/PrintingTriangle.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PrintingTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        printTriangle(n);
+
+        scanner.close();
+    }
+
+    public static void printTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            printLine(1, i);
+        }
+        for (int i = n - 1; i > 0; i--) {
+            printLine(1, i);
+        }
+    }
+
+    public static void printLine(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
